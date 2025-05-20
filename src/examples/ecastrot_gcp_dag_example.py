58d236e8b8cbaa9ca9a4a8870a2b0da1b5824424
smu-ecastrot <ecastrot@unidata.cl>
2025-05-20 09:01:23 -0400

    Testing if executable file is located

--- src/examples/ecastrot_gcp_dag_example.py
+++ src/examples/ecastrot_gcp_dag_example.py
@@ -52,11 +52,11 @@ with DAG(**dag_args) as dag:
                     'gcp_dag_example.py'
                 ),
                 # Common library
-                'python_file_uris': [(
-                    'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
-                    'src/'
-                    'common/'
-                )],
+                #'python_file_uris': [(
+                #    'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                #    'src/'
+                #    'common/'
+                #)],
                 # For Google Big Query read/write
                 'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.41.0.jar'],
                 # Main file arguments
