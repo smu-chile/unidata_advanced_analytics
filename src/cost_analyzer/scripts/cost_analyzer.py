2d7fcb242cfa0c5bdd217a5efe10d055941f6616
smu-ecastrot <ecastrot@unidata.cl>
2025-05-28 16:13:25 -0400

    Now uses INFORMATION_SCHEMA.JOBS view and can be used in various projects

--- src/cost_analyzer/scripts/cost_analyzer.py
+++ src/cost_analyzer/scripts/cost_analyzer.py
@@ -3,7 +3,6 @@ import os
 import logging
 import argparse
 from logging import config
-from collections import defaultdict
 
 # pip
 import pandas as pd
@@ -13,6 +12,7 @@ from google.cloud import bigquery
 # Own
 import common.gcp_extended.bigquery as gbq_extended
 from common.constants import LOGGING_CONFIG
+from common.utils.queries import QueryDict
 
 
 # -------------------------------------------------------------------------
@@ -33,111 +33,167 @@ parser.add_argument(
 
 
 # -------------------------------------------------------------------------
+#  SQL Queries
+# -------------------------------------------------------------------------
+SQL_QUERIES = QueryDict({
+    'project_jobs':
+    """
+    SELECT
+        cache_hit,
+        creation_time,
+        end_time,
+        job_id,
+        job_type,
+        labels,
+        project_id,
+        state,
+        statement_type,
+        COALESCE(total_bytes_billed, 0) AS total_bytes_billed,
+        COALESCE(total_bytes_processed, 0) AS total_bytes_processed,
+        COALESCE(total_slot_ms, 0) AS total_slot_ms,
+        user_email
+
+    FROM `${gcp_project}`.`region-${gcp_region}`.INFORMATION_SCHEMA.JOBS
+
+    WHERE
+        TIMESTAMP("${min_creation_time}") <= CREATION_TIME
+        AND CREATION_TIME <= TIMESTAMP("${min_creation_time}") + INTERVAL 1 DAY
+        AND STATEMENT_TYPE != 'SCRIPT'
+    """
+})
+
+
+# -------------------------------------------------------------------------
 # Functions and Classes
 # -------------------------------------------------------------------------
-def replaceUserLabel(row: pd.Series) -> str:
-    """"""
-    valid_substitution_users = {
-        'ecastrot@unidata.cl': 'ecastrot',
-        'csotob@unidata.cl': 'csotob',
-        'rarriagada@unidata.cl': 'rarriagada',
-        'bmolinab@unidata.cl': 'bmolinab',
-        'csmunozr@unidata.cl': 'csmunozr',
-        'e_ebenites@smu.cl': 'e_ebenites',
-        'sabbas@smu.cl': 'sabbas',
-    }
+def setUserFromMail(row: pd.Series) -> str:
+    """Gets user from email if project name was not set by labels.
 
-    if row['user'] != 'default':
-        return row['user']
+    Parameters
+    ----------
+    row : pd.Series
+        Row with user emails and user or project names as columns
 
-    return valid_substitution_users.get(row['user_email'], 'default')
+    Returns
+    -------
+    user_or_project_name : str
+        Name of the user or project that made the query
+    """
+    if not pd.isna(row['user_or_project']):
+        return row['user_or_project']
+    return row['user_email'].split('@')[0]
 
 
 # -------------------------------------------------------------------------
 # Main function
 # -------------------------------------------------------------------------
 def main() -> None:  # noqa: D103
+    user = 'cost_analyzer'
     # Parse input variables
     args = vars(parser.parse_args())
     execution_date: str = args['execution_date']
-    gcp_project: str = args['project_id']
+    gcp_project_id: str = args['project_id']
 
-    # Get all jobs in the execution_date
-    logging.info(f'Getting all jobs started on {execution_date}')
+    gcp_projects = [
+        'cl-bigdata-analytics',
+        #'cl-bigdata-analytics-preprod',
+        #'cl-bigdata-analytics-prod',
+        #'cl-cda-unidata-dev',
+        #'cl-cda-unidata-prod',
+    ]
+
+    # Set gbq client for all subsequent queries
     gbq_client = bigquery.Client()
-    all_jobs = gbq_client.list_jobs(
-        project=gcp_project,
-        all_users=True,
-        min_creation_time=pendulum.datetime(
-            *map(int, execution_date.split('-')),
-            hour=0, minute=0, second=0, microsecond=0,
-            tz=pendulum.timezone('America/Santiago')
-        ),
-        max_creation_time=pendulum.datetime(
-            *map(int, execution_date.split('-')),
-            hour=23, minute=59, second=59, microsecond=999999,
-            tz=pendulum.timezone('America/Santiago')
-        ),
-    )
 
-    # Save job info into dict[str, list]
-    logging.debug('Start parsing jobs')
-    jobs_info = defaultdict(list)
-    for job in all_jobs:
-        jobs_info['job_id'].append(job.job_id)
-        jobs_info['job_type'].append(job.job_type)
-        jobs_info['state'].append(job.state)
-        jobs_info['statement_type'].append(job.statement_type)
+    # Scan jobs
+    jobs = pd.DataFrame()
 
-        duration = pendulum.instance(job.ended) - pendulum.instance(job.created)
-        jobs_info['duration'].append(f'{duration.hours}:{duration.minutes}:{duration.seconds}.{duration.microseconds}')
-
-        jobs_info['user'].append(job.labels.get('user', 'default'))
-        jobs_info['user_email'].append(job.user_email)
-
-        jobs_info['total_bytes_processed'].append(
-            job.total_bytes_processed if job.total_bytes_processed is not None else 0
-        )
-        jobs_info['total_bytes_billed'].append(
-            job.total_bytes_billed if job.total_bytes_billed is not None else 0
+    for gcp_project in gcp_projects:
+        # Read INFORMATION_SCHEMA.JOBS view
+        project_jobs = gbq_extended.readBigQuery(
+            query=SQL_QUERIES['project_jobs'].substitute(
+                gcp_project=gcp_project,
+                # Using US general region to catch all jobs
+                gcp_region='us',
+                min_creation_time=pendulum.datetime(
+                    *map(int, execution_date.split('-')),
+                    hour=0, minute=0, second=0, microsecond=0,
+                    tz=pendulum.timezone('America/Santiago')
+                ),
+            ),
+            user=user,
+            gbq_client=gbq_client,
         )
 
-    # dict[str, list] into DataFrame
-    jobs_info = pd.DataFrame(jobs_info)
-    logging.debug('Ended parsing jobs')
-    # Break execution when no job was executed
-    if jobs_info.empty:
-        logging.info(f'No jobs founded for {execution_date}. Breaking execution')
-        return
-    logging.info(f'{jobs_info.shape[0]:,} jobs founded')
+        # Break execution when no job was executed
+        if project_jobs.empty:
+            logging.info(f'No jobs founded for {execution_date}. Going to the next project')
+            continue
+        logging.info(f'{project_jobs.shape[0]:,} jobs founded in {gcp_project} project')
 
-    # Assign start date
-    jobs_info['started_date'] = execution_date
 
-    # Transform bytes to mega bytes
-    jobs_info['total_bytes_billed'] = jobs_info['total_bytes_billed'].fillna(0) / 1e6
-    jobs_info['total_bytes_processed'] = jobs_info['total_bytes_processed'].fillna(0) / 1e6
-    jobs_info = jobs_info.rename(columns={
-        'total_bytes_billed': 'total_mb_billed',
-        'total_bytes_processed': 'total_mb_processed',
-    })
+        # Compute query duration
+        project_jobs['creation_time'] = project_jobs['creation_time'].dt.tz_convert(
+            'America/Santiago'
+        )
+        project_jobs['end_time'] = project_jobs['end_time'].dt.tz_convert(
+            'America/Santiago'
+        )
+        project_jobs['duration'] = (
+            project_jobs['end_time'] - project_jobs['creation_time']
+        ).astype(str).str.split(' ').str[-1]
 
-    # Build user name from known users
-    # This is made to flag queries made by known users directly on the GCP
-    # console
-    jobs_info['user'] = jobs_info[['user', 'user_email']].apply(replaceUserLabel, axis=1)
+        # Get date of the execution
+        project_jobs['creation_date'] = project_jobs['creation_time'].dt.date
+
+        # bytes to Mbytes
+        project_jobs['total_mb_billed'] = project_jobs['total_bytes_billed'] / 1024 / 1024
+        project_jobs['total_mb_processed'] = project_jobs['total_bytes_processed'] / 1024 / 1024
+        # Slot usage ms to s
+        project_jobs['total_s_slot_usage'] = project_jobs['total_slot_ms'] / 1000 / 60
+
+        # Construct user
+        project_jobs['user_or_project'] = project_jobs['labels'].str[0].str.get('value')
+        project_jobs['user_or_project'] = project_jobs[[
+            'user_email', 'user_or_project'
+        ]].apply(
+            setUserFromMail,
+            axis=1
+        )
+
+        jobs = pd.concat([
+                jobs,
+                project_jobs[[
+                    'project_id',
+                    'user_or_project',
+                    'user_email',
+                    'job_id',
+                    'job_type',
+                    'cache_hit',
+                    'creation_date',
+                    'duration',
+                    'statement_type',
+                    'state',
+                    'total_mb_billed',
+                    'total_mb_processed',
+                    'total_s_slot_usage'
+                ]]
+            ],
+            axis=0,
+            ignore_index=True,
+        )
 
     # Create GBQ table if does not exist
     logging.info('Creating GBQ table using JSON')
     gbq_extended.createTableFromJSON(
         ddl_json_config_path=os.path.join('gbq_objects', 'gbq_job_consumption.json'),
-        project=gcp_project,
+        project=gcp_project_id,
         if_exists='ignore'
     )
 
     # Delete data from the execution_date if reprocessing
     gbq_extended.deleteFromTable(
-        table_ref=f'{gcp_project}.ML_LAB.GBQ_JOB_CONSUMPTION',
+        table_ref=f'{gcp_project_id}.ML_LAB.GBQ_JOB_CONSUMPTION',
         column_name='started_date',
         column_type='date',
         column_value=execution_date,
@@ -146,11 +202,8 @@ def main() -> None:  # noqa: D103
     # Upload data
     logging.info(f'Uploading data for {execution_date}')
     gbq_extended.uploadFrame(
-        jobs_info[[
-            'job_id', 'job_type', 'state', 'statement_type', 'started_date', 'duration',
-            'user', 'user_email', 'total_mb_billed', 'total_mb_processed'
-        ]],
-        table_ref=f'{gcp_project}.ML_LAB.GBQ_JOB_CONSUMPTION',
+        jobs,
+        table_ref=f'{gcp_project_id}.ML_LAB.GBQ_JOB_CONSUMPTION',
         table_ddl_json_path=os.path.join('gbq_objects', 'gbq_job_consumption.json'),
         if_exists='append',
     )
