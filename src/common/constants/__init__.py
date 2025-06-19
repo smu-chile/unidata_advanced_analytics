2cb2351b230e98314a885fcf9e04d3c4d792c1af
smu-ecastrot <ecastrot@unidata.cl>
2025-06-19 10:17:26 -0400

    Add MART banner numbers to constants

--- src/common/constants/__init__.py
+++ src/common/constants/__init__.py
@@ -13,3 +13,9 @@ with open(os.path.join(
     'short_banners.json'
 )) as __short_banners_file:
     SHORT_STORE_BANNERS = json.load(__short_banners_file)
+
+with open(os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'banner_number.json'
+)) as __logging_config_file:
+    BANNER_NUMBER = json.load(__logging_config_file)
