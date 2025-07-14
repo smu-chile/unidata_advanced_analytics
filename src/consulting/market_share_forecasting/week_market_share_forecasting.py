02ca479513863776faf3dedb18859eb4c456ec61
smu-ecastrot <ecastrot@unidata.cl>
2025-07-14 15:04:02 -0400

    Merge both DAGs

--- src/consulting/market_share_forecasting/week_market_share_forecasting.py
+++ src/consulting/market_share_forecasting/week_market_share_forecasting.py
@@ -41,8 +41,11 @@ dag_args = {
 with DAG(**dag_args) as dag:
     EXECUTION_DATE = "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_end).strftime('%Y-%m-%d')) }}"  # noqa: E501
 
-    get_costs = DataprocCreateBatchOperator(
-        task_id = 'get_costs',
+    # ---------------------------------------------------------------------
+    # Week forecasting
+    # ---------------------------------------------------------------------
+    week_forecasting = DataprocCreateBatchOperator(
+        task_id = 'week_forecasting',
 
         batch = {
             'pyspark_batch': {
@@ -98,4 +101,72 @@ with DAG(**dag_args) as dag:
         project_id = GCP_PROJECT_ID,
     )
 
-get_costs  # noqa: B018
+
+    # ---------------------------------------------------------------------
+    # Day forecasting
+    # ---------------------------------------------------------------------
+    day_forecasting = DataprocCreateBatchOperator(
+        task_id = 'day_forecasting',
+
+        batch = {
+            'pyspark_batch': {
+                # Main file to run in the dataproc pod
+                'main_python_file_uri': (
+                    'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                    'consulting/'
+                    f'{PROJECT_NAME}/'
+                    'scripts/'
+                    'day_compute_market_share.py'
+                ),
+                # Common files
+                'python_file_uris': [
+                    (
+                        'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                        'common/'
+                    ),
+                    (
+                        'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                        'consulting/'
+                        f'{PROJECT_NAME}/'
+                        'gbq_objects'
+                    )
+                ],
+                # For Google Big Query read/write
+                'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.42.2.jar'],
+                # Main file arguments
+                'args': [
+                    '--project_name', PROJECT_NAME,
+                    '--gcp_project', GCP_PROJECT_ID,
+                    '--execution_date', EXECUTION_DATE,
+                ],
+            },
+
+            # Docker image to be used in the dataproc pod
+            'runtime_config': {
+                'version': '2.2',
+                'container_image': (
+                    'us-east1-docker.pkg.dev/'
+                    f'{GCP_PROJECT_ID}/'
+                    'dataproc-worker-images/'
+                    f"{PROJECT_NAME.replace('_', '-')}:latest"
+                ),
+            },
+
+            # Privileges config
+            'environment_config': {
+                'execution_config': {
+                    'service_account': '{{ var.value.develop_smu_unidata_dataproc_sa }}',
+                    'network_uri': '{{ var.value.develop_smu_unidata_dataproc_network }}',
+                    'subnetwork_uri': '{{ var.value.develop_smu_unidata_dataproc_subnetwork }}',
+                    'ttl': '14400s',
+                },
+            },
+        },
+
+        # Batch ID
+        batch_id = 'batch-{{ macros.uuid.uuid4() }}',
+        project_id = GCP_PROJECT_ID,
+    )
+
+
+[week_forecasting, day_forecasting]  # noqa: B018
