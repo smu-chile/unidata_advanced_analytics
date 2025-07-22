4eb9c45984ab393dc37505fed56f9b1938228dc4
csotob-smu <csotob@smu.cl>
2025-07-22 09:55:48 -0400

    MOD: fix data type oops

--- src/ingest/scripts/ingest_nielsen_anual.py
+++ src/ingest/scripts/ingest_nielsen_anual.py
@@ -71,7 +71,7 @@ def main() -> None:  # noqa: D103
     args = vars(parser.parse_args())
     gcp_project_id: str = args['project_id']
     proc_years: list[str] = args['proc_years'].split(':')
-    load_files = ('jerarquia_ytd')
+    load_files = ['jerarquia_ytd']
     # Set all clients
     sp_cred = secretmanager.getSecret('bdaa_sharepoint_credentials')
     gbq_client = bigquery.Client()
