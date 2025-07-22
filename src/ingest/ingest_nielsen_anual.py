d17192224b50aac45635ed75868e80b7091a285b
csotob-smu <csotob@smu.cl>
2025-07-22 09:24:41 -0400

    ADD: ingesta data nielsen anual v1

---
+++ src/ingest/ingest_nielsen_anual.py
@@ -0,0 +1,104 @@
+# Default
+from datetime import timedelta
+
+# pip
+import pendulum
+from airflow.models import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateBatchOperator,
+)
+
+
+# Globals
+PROJECT_NAME = 'ingest'
+GCP_PROJECT_ID =  '{{ var.value.develop_smu_unidata_default_project_id }}'
+
+dag_args = {
+    'dag_id': 'ingest_data_nielsen_anual',
+    'schedule_interval': None,
+    'dagrun_timeout': None,
+    'catchup': True,
+    'max_active_runs': 1,
+    'concurrency': 1,
+    'tags': [PROJECT_NAME, 'csotob'],
+    'default_args': {
+        'project_id': GCP_PROJECT_ID,
+        'region': '{{ var.value.develop_smu_unidata_default_region }}',
+        'owner': 'BIGDATA_ANALYTICS',
+        'email': ['csotob@unidata.cl'],
+        'start_date': pendulum.datetime(
+            2025, 6, 25,
+            tz=pendulum.timezone('America/Santiago')
+        ),
+        'depends_on_past': False,
+        'catchup': False,
+        'email_on_failure': True,
+        'email_on_retry': False,
+        'retries': 0,
+        'retry_delay': timedelta(minutes=5)
+    }
+}
+
+with DAG(**dag_args) as dag:
+    EXECUTION_DATE = "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_end).strftime('%Y-%m-%d')) }}"  # noqa: E501
+    PROC_YEARS = "{{ dag_run.conf.get('proc_years', dag.timezone.convert(data_interval_end).strftime('%Y')) }}"  # noqa: E501
+
+    ingest_nielsen_anual = DataprocCreateBatchOperator(
+        task_id = 'ingest_nielsen_anual',
+
+        batch = {
+            'pyspark_batch': {
+                # Main file to run in the dataproc pod
+                'main_python_file_uri': (
+                    'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                    f'{PROJECT_NAME}/'
+                    'scripts/'
+                    'ingest_nielsen_anual.py'
+                ),
+                # Common files
+                'python_file_uris': [
+                    (
+                        'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                        'common/'
+                    ),
+                    (
+                        'gs://{{ var.value.develop_smu_unidata_dataproc_scripts_storage }}/'
+                        f'{PROJECT_NAME}/'
+                        'gbq_objects/'
+                    )
+                ],
+                # For Google Big Query read/write
+                'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.42.2.jar'],
+                # Main file arguments
+                'args': [
+                    '--project_id', GCP_PROJECT_ID,
+                    '--execution_date', EXECUTION_DATE,
+                    '--proc_years', PROC_YEARS
+                ],
+            },
+            # Docker image to be used in the dataproc pod
+            'runtime_config': {
+                'version': '2.2',
+                'container_image': (
+                    'us-east1-docker.pkg.dev/'
+                    f'{GCP_PROJECT_ID}/'
+                    'dataproc-worker-images/'
+                    f"{PROJECT_NAME.replace('_', '-')}:latest"
+                ),
+            },
+
+            # Privileges config
+            'environment_config': {
+                'execution_config': {
+                    'service_account': '{{ var.value.develop_smu_unidata_dataproc_sa }}',
+                    'network_uri': '{{ var.value.develop_smu_unidata_dataproc_network }}',
+                    'subnetwork_uri': '{{ var.value.develop_smu_unidata_dataproc_subnetwork }}',
+                    'ttl': '14400s',
+                },
+            },
+        },
+
+        # Batch ID
+        batch_id = 'batch-{{ macros.uuid.uuid4() }}',
+        project_id = GCP_PROJECT_ID,
+    )
---
+++ src/ingest/scripts/ingest_nielsen_anual.py
@@ -0,0 +1,119 @@
+# Default
+import os
+import logging
+import argparse
+from string import Template
+from logging import config
+
+# pip
+from google.cloud import bigquery
+
+import common.gcp_extended.bigquery as gbq_extended
+import common.gcp_extended.secretsmanager as secretmanager
+
+# Own
+import common.office365_extended.sharepoint as sp
+from common.constants import LOGGING_CONFIG
+
+
+# -------------------------------------------------------------------------
+#  Config
+# -------------------------------------------------------------------------
+# Logging config
+config.dictConfig(LOGGING_CONFIG)
+# Parser config
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--project_id', type=str,
+    help='GCP project in which the script will be executed'
+)
+parser.add_argument(
+    '--execution_date', type=str,
+    help='DAG execution date'
+)
+parser.add_argument(
+    '--proc_years', type=str,
+    help='Years to process'
+)
+# -------------------------------------------------------------------------
+# Cleaning Func
+# -------------------------------------------------------------------------
+def cleaning_func(df, year):
+    print('Before cleaning:', df)
+    #Drop first 8 rows and empty right columns
+    df = df[9:]  # noqa: PD901
+    df = df.iloc[:, :15]  # noqa: PD901
+    logging.info('shape: %s', df.shape)
+    #rename columns
+    df.columns = ['departamento','cl_xc_categoria', 'segmento',
+                 'negocio', 'item_code', 'item', 'subsegmento',
+                 'tamano', 'tipo', 'variedad', 'periodos',
+                 'total_mercado_vtas_valor', 'total_mercado_vtas_unit',
+                 'total_supermercados_amp_internet_vtas_valor',
+                'total_supermercados_amp_internet_vtas_unit'
+                 ]
+    #Drop trailing rows
+    df = df.dropna(axis=0,subset=['departamento','cl_xc_categoria'])  # noqa: PD901
+    df = df.replace('|', '', regex=True)  # noqa: PD901
+    #Add column out of periodos
+    df['periodos'] = df['periodos'].str.lower()#
+    df['year'] = year
+    df = df.replace(' nan', 0.0)  # noqa: PD901
+    df = df.replace('nan', 0.0)  # noqa: PD901
+    print('After cleaning:', df)
+    return df
+
+# -------------------------------------------------------------------------
+# Main function
+# -------------------------------------------------------------------------
+def main() -> None:  # noqa: D103
+    # Parse input variables
+    args = vars(parser.parse_args())
+    gcp_project_id: str = args['project_id']
+    proc_years: list[str] = args['proc_years'].split(':')
+    load_files = ('jerarquia_ytd')
+    # Set all clients
+    sp_cred = secretmanager.getSecret('bdaa_sharepoint_credentials')
+    gbq_client = bigquery.Client()
+    site_root = (
+    '/sites/'
+    'BigDatayAdvancedAnalytics/'
+    'Documentos compartidos/'
+    'Pricing/Forecast Venta/'
+    'Data Nielsen/Uni M10 Internet/'
+    )
+    #input files
+    input_files = {
+        'jerarquia_ytd' :  Template('$site_root/Jerarquia Mercado YTD/VENTA MERCADO JERARQUIA NIELSEN $year.xlsx')  # noqa: E501
+       }
+    #table definitions jsons
+    jsons = {
+        'jerarquia_ytd' : 'nielsen_anual_venta_mercado_jerarquia_ytd.json'
+
+    }
+
+    for file in load_files:
+        for year in proc_years:
+            input_file_ytd = input_files[file].substitute(site_root=site_root,
+                                                          year=year)
+            logging.info(f'Starting extraction of {input_file_ytd} from Sharepoint')
+            sharepoint = sp.SharePointFile(sp_cred['client_id'],
+                                       sp_cred['client_secret'],input_file_ytd)
+            df_file = sharepoint.toFrame()
+            df_file =cleaning_func(df_file,year)
+
+            # Upload data
+            logging.info('Uploading data')
+            gbq_extended.uploadFrame(
+                df_file,
+                table_ddl_json_path=os.path.join('gbq_objects', jsons[file]),
+                project=gcp_project_id,
+                gbq_client=gbq_client,
+                if_exists='append',
+            )
+            logging.info(f'File {input_file_ytd} uploaded')
+    logging.info('Process ended!')
+
+
+if __name__ == '__main__':
+    main()
