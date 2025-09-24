4dd488f8b68164ea57213bb1e6b89932bc4b7367
csotob-smu <csotob@smu.cl>
2025-09-24 14:24:00 -0300

    testitest 4

--- src/ingest/sftp/salesforce_sms.py
+++ src/ingest/sftp/salesforce_sms.py
@@ -117,6 +117,7 @@ with DAG(**dag_args) as dag:
         # Batch ID
         batch_id = BATCH_ID,
         project_id = GCP_PROJECT_ID,
+        asynchronous = True,
         deferrable = True,
         do_xcom_push=True
     )
