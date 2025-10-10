32a4a630a86f6faf807876e1a0f07c263e6995df
smu-ecastrot <ecastrot@unidata.cl>
2025-10-10 13:11:38 -0300

    Minor fix in imports

--- src/consulting/market_share_forecasting/market_share_forecasting.py
+++ src/consulting/market_share_forecasting/market_share_forecasting.py
@@ -66,6 +66,12 @@ with DAG(**dag_args) as dag:
                         f'gs://{dag_env_config["scripts_gcs"]}/'
                         'common/'
                     )
+                    (
+                        f'gs://{dag_env_config["scripts_gcs"]}/'
+                        'consulting/'
+                        f'{PROJECT_NAME}/'
+                        'gbq_objects'
+                    )
                 ],
                 # For Google Big Query read/write
                 'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.42.2.jar'],
