1fd7ce86275076aabb5065544dad57a75a9e2188
smu-ecastrot <ecastrot@unidata.cl>
2025-11-07 12:07:17 -0300

    Fix file name and data interval

--- src/etl_aws/long_term_control_group.py
+++ src/etl_aws/long_term_control_group.py
@@ -43,7 +43,7 @@ dag_args = {
 }
 
 with DAG(**dag_args) as dag:
-    EXECUTION_DATE = "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_start).strftime('%Y-%m-%d')) }}"  # noqa: E501
+    EXECUTION_DATE = "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_end).strftime('%Y-%m-%d')) }}"  # noqa: E501
 
     move_long_term_control_group = DataprocCreateBatchOperator(
         task_id = 'move_long_term_control_group',
