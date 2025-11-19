4a1f3510b715b557f2f67a5eab283778c5cb8af1
Benjamin <bmolinab@unidata.cl>
2025-11-19 12:00:51 -0300

    Script y dag de elasticidad

--- src/pricing/balance_matrix.py
+++ src/pricing/balance_matrix.py
@@ -29,6 +29,9 @@ start_date = [2025, 6, 20]
 script1 = 'product_sensibility'
 # Task 2
 script2 = 'processed_regression_data'
+# Task 3
+script3 = 'product_elasticity'
+
 
 store_banner_list = ['Unimarc', 'Alvi']
 
@@ -68,6 +71,7 @@ with DAG(**dag_args) as dag:
 
     sensibility_tasks = []
     processed_data_tasks = []
+    elasticity_tasks = []
 
     for store_banner in store_banner_list:
         banner_suffix = store_banner.replace(' ', '_').lower()
@@ -212,9 +216,77 @@ with DAG(**dag_args) as dag:
             project_id=dag_env_config['project_id'],
         )
 
+
+        # ---------- Task script3: product_elasticity ----------
+        elasticity_task = DataprocCreateBatchOperator(
+            task_id=f'{script3}_{banner_suffix}',
+            batch={
+                'pyspark_batch': {
+                    'main_python_file_uri': (
+                        f'gs://{dag_env_config["scripts_gcs"]}/'
+                        f'{PROJECT_NAME}/'
+                        'scripts/'
+                        f'{script3}.py'
+                    ),
+                    'python_file_uris': [
+                        (
+                            f'gs://{dag_env_config["scripts_gcs"]}/'
+                            'common/'
+                        ),
+                        (
+                            f'gs://{dag_env_config["scripts_gcs"]}/'
+                            f'{PROJECT_NAME}/'
+                            'gbq_objects/'
+                        ),
+                    ],
+                    'jar_file_uris': [
+                        'gs://spark-lib/bigquery/'
+                        'spark-3.5-bigquery-0.42.2.jar'
+                    ],
+                    'args': [
+                        '--project_id',
+                        dag_env_config['project_id'],
+                        '--execution_date',
+                        EXECUTION_DATE,
+                        '--store_banner',
+                        store_banner,
+                        '--use',
+                        'ELASTICITY',
+                    ],
+                },
+                'runtime_config': {
+                    'version': '2.2',
+                    'container_image': (
+                        'us-east1-docker.pkg.dev/'
+                        f'{dag_env_config["project_id"]}/'
+                        'dataproc-worker-images/'
+                        f"{PROJECT_NAME.replace('_', '-')}:latest"
+                    ),
+                    'properties': {
+                        'spark.executor.instances': '2',
+                        'spark.executor.cores': '4',
+                        'spark.executor.memory': '4096m',
+                        'spark.driver.cores': '4',
+                        'spark.driver.memory': '10g',
+                    },
+                },
+                'environment_config': {
+                    'execution_config': {
+                        'service_account': dag_env_config['g_service_account'],
+                        'network_uri': dag_env_config['network'],
+                        'subnetwork_uri': dag_env_config['subnetwork'],
+                        'ttl': '14400s',
+                    },
+                },
+            },
+            batch_id='batch-{{ macros.uuid.uuid4() }}',
+            project_id=dag_env_config['project_id'],
+        )
+
         # Dependencia por formato: primero script1, luego script2
-        sensibility_task >> processed_data_task
+        sensibility_task >> processed_data_task >> elasticity_task
 
         sensibility_tasks.append(sensibility_task)
         processed_data_tasks.append(processed_data_task)
+        elasticity_tasks.append(elasticity_task)
 
