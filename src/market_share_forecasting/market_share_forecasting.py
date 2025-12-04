790e32cb77af03ea075f7763efc09723ce2ea08a
smu-ecastrot <ecastrot@unidata.cl>
2025-12-04 16:53:22 -0300

    Better, hotter, sexier, nastier way to definining DAGs

--- src/market_share_forecasting/market_share_forecasting.py
+++ src/market_share_forecasting/market_share_forecasting.py
@@ -1,13 +1,28 @@
 # Default
 import json
+import platform
+import importlib
 from datetime import timedelta
 
 # pip
+import pendulum
 from airflow.models import DAG
 from airflow.configuration import conf
-from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocCreateBatchOperator,
-)
+
+
+if platform.system() == 'Windows':
+    from common.operators.dataproc_create_batch import (
+        ExtendedDataprocCreateBatchOperator,
+    )
+elif platform.system() == 'Linux':
+    ExtendedDataprocCreateBatchOperator = (importlib.import_module(
+        'dev.'
+        'smu-chile.unidata_advanced_analytics.'
+        'src.common.operators.dataproc_create_batch'
+    )).ExtendedDataprocCreateBatchOperator
+else:
+    err_msg = ''
+    raise NotImplementedError(err_msg)
 
 
 # Globals
@@ -32,7 +47,10 @@ dag_args = {
         'region': dag_env_config['region'],
         'owner': 'BIGDATA_ANALYTICS',
         'email': ['ecastrot@unidata.cl'],
-        'start_date': None,
+        'start_date': pendulum.datetime(
+            2025, 1, 1,
+            tz=pendulum.timezone('America/Santiago')
+        ),
         'depends_on_past': False,
         'email_on_failure': True,
         'email_on_retry': False,
@@ -47,136 +65,49 @@ with DAG(**dag_args) as dag:
     # ---------------------------------------------------------------------
     # Week forecasting
     # ---------------------------------------------------------------------
-    week_forecasting = DataprocCreateBatchOperator(
-        task_id = 'week_forecasting',
-
-        batch = {
-            'pyspark_batch': {
-                # Main file to run in the dataproc pod
-                'main_python_file_uri': (
-                    f'gs://{dag_env_config["scripts_gcs"]}/'
-                    f'{PROJECT_NAME}/'
-                    'scripts/'
-                    'week_compute_market_share.py'
-                ),
-                # Common files
-                'python_file_uris': [
-                    (
-                        f'gs://{dag_env_config["scripts_gcs"]}/'
-                        'common/'
-                    ),
-                    (
-                        f'gs://{dag_env_config["scripts_gcs"]}/'
-                        f'{PROJECT_NAME}/'
-                        'gbq_objects'
-                    )
-                ],
-                # For Google Big Query read/write
-                'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.42.2.jar'],
-                # Main file arguments
-                'args': [
-                    '--project_name', PROJECT_NAME,
-                    '--gcp_project', dag_env_config['project_id'],
-                    '--execution_date', EXECUTION_DATE,
-                ],
-            },
-
-            # Docker image to be used in the dataproc pod
-            'runtime_config': {
-                'version': '2.2',
-                'container_image': (
-                    'us-east1-docker.pkg.dev/'
-                    f'{dag_env_config["project_id"]}/'
-                    'dataproc-worker-images/'
-                    f"{PROJECT_NAME.replace('_', '-')}:latest"
-                ),
-            },
-
-            # Privileges config
-            'environment_config': {
-                'execution_config': {
-                    'service_account': dag_env_config['g_service_account'],
-                    'network_uri': dag_env_config['network'],
-                    'subnetwork_uri': dag_env_config['subnetwork'],
-                    'ttl': '43200s',
-                },
-            },
-        },
-
-        # Leaves Airflow Trigger to track the status of the Dataproc batch
-        deferrable=True,
-
-        # Batch ID
-        batch_id = 'batch-{{ macros.uuid.uuid4() }}',
-        project_id = dag_env_config['project_id'],
+    week_forecasting = ExtendedDataprocCreateBatchOperator(
+        task_id='week_forecasting',
+        project_name=PROJECT_NAME,
+        python_script_path=(
+            f'{PROJECT_NAME}/'
+            'scripts/'
+            'week_compute_market_share.py'
+        ),
+        dag_env_config=dag_env_config,
+        pyspark_batch_args=[
+            '--project_name', PROJECT_NAME,
+            '--gcp_project', dag_env_config['project_id'],
+            '--execution_date', EXECUTION_DATE,
+        ],
+        include_paths=[
+            'common/',
+            f'{PROJECT_NAME}/gbq_objects/'
+        ],
+        ttl=43200,
     )
 
 
     # ---------------------------------------------------------------------
     # Day forecasting
     # ---------------------------------------------------------------------
-    day_forecasting = DataprocCreateBatchOperator(
-        task_id = 'day_forecasting',
-
-        batch = {
-            'pyspark_batch': {
-                # Main file to run in the dataproc pod
-                'main_python_file_uri': (
-                    f'gs://{dag_env_config["scripts_gcs"]}/'
-                    f'{PROJECT_NAME}/'
-                    'scripts/'
-                    'day_compute_market_share.py'
-                ),
-                # Common files
-                'python_file_uris': [
-                    (
-                        f'gs://{dag_env_config["scripts_gcs"]}/'
-                        'common/'
-                    ),
-                    (
-                        f'gs://{dag_env_config["scripts_gcs"]}/'
-                        f'{PROJECT_NAME}/'
-                        'gbq_objects'
-                    )
-                ],
-                # For Google Big Query read/write
-                'jar_file_uris': ['gs://spark-lib/bigquery/spark-3.5-bigquery-0.42.2.jar'],
-                # Main file arguments
-                'args': [
-                    '--project_name', PROJECT_NAME,
-                    '--gcp_project', dag_env_config['project_id'],
-                    '--execution_date', EXECUTION_DATE,
-                ],
-            },
-
-            # Docker image to be used in the dataproc pod
-            'runtime_config': {
-                'version': '2.2',
-                'container_image': (
-                    'us-east1-docker.pkg.dev/'
-                    f'{dag_env_config["project_id"]}/'
-                    'dataproc-worker-images/'
-                    f"{PROJECT_NAME.replace('_', '-')}:latest"
-                ),
-            },
-
-            # Privileges config
-            'environment_config': {
-                'execution_config': {
-                    'service_account': dag_env_config['g_service_account'],
-                    'network_uri': dag_env_config['network'],
-                    'subnetwork_uri': dag_env_config['subnetwork'],
-                    'ttl': '14400s',
-                },
-            },
-        },
-
-        # Leaves Airflow Trigger to track the status of the Dataproc batch
-        deferrable=True,
-
-        # Batch ID
-        batch_id='batch-{{ macros.uuid.uuid4() }}',
-        project_id=dag_env_config['project_id'],
+    day_forecasting = ExtendedDataprocCreateBatchOperator(
+        task_id='day_forecasting',
+        project_name=PROJECT_NAME,
+        python_script_path=(
+            f'{PROJECT_NAME}/'
+            'scripts/'
+            'day_compute_market_share.py'
+        ),
+        dag_env_config=dag_env_config,
+        pyspark_batch_args=[
+            '--project_name', PROJECT_NAME,
+            '--gcp_project', dag_env_config['project_id'],
+            '--execution_date', EXECUTION_DATE,
+        ],
+        include_paths=[
+            'common/',
+            f'{PROJECT_NAME}/gbq_objects/'
+        ],
     )
 
 
