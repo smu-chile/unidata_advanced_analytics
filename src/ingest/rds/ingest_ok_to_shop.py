8f46dffab193f8ddcf8cd6ef42d4c1c993a71a6a
smu-ecastrot <ecastrot@unidata.cl>
2025-12-10 10:58:57 -0300

    Better fix

--- src/ingest/rds/ingest_ok_to_shop.py
+++ src/ingest/rds/ingest_ok_to_shop.py
@@ -72,7 +72,7 @@ with DAG(**dag_args) as dag:
             'ingest_ok_to_shop.py'
         ),
         dag_env_config=dag_env_config,
-        docker_image_name=f"{PROJECT_NAME.replace('_', '-')}-{SUBPROJECT_NAME}",
+        docker_image_name=f'{PROJECT_NAME}-{SUBPROJECT_NAME}',
         pyspark_batch_args=[
             '--project_id', dag_env_config['project_id'],
             '--execution_date', EXECUTION_DATE,
