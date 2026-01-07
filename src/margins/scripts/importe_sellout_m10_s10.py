fee9092a0297127752ed504f29d81f6daf811965
csotob-smu <csotob@smu.cl>
2026-01-07 09:46:24 -0300

    remove todo bc its done

--- src/margins/scripts/importe_sellout_m10_s10.py
+++ src/margins/scripts/importe_sellout_m10_s10.py
@@ -106,8 +106,6 @@ def main() -> None:  # noqa: D103
         '/sites/BigDatayAdvancedAnalytics/Documentos compartidos/'
         'Pricing/SellOut/SellOut Consolidado/'
     )
-    #TODO(csotob): Cambiar nombre de input:file y la logica de renombrar a
-    # 'Procesado-' en GCP cuando se depreque en aws, eventualmente
     input_files = {
         's10': f'{file_site}/S10/Sellout_S10_{execution_month}.xlsx',
         'm10': f'{file_site}/M10/Sellout_M10_{execution_month}.xlsx',
