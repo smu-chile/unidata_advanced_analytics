eefba8034b96af13dd130f31898fdf9d6112aeaf
csotob-smu <csotob@smu.cl>
2026-01-20 16:14:19 -0300

    Prosepan v1 c:

---
+++ src/providers/scripts/providers_prosepan.py
@@ -0,0 +1,164 @@
+"""Script Creación Consolidado y egesta hacia sftp.
+"""
+# Default
+import io
+import os
+import logging
+import argparse
+from logging import config
+
+import pandas as pd
+
+# pip
+from google.cloud.bigquery import Client
+
+import common.gcp_extended.secretsmanager as secretmanager
+import common.office365_extended.sharepoint as sp
+
+# Own
+from common.constants import LOGGING_CONFIG
+from common.gcp_extended.bigquery import readBigQuery
+
+
+# -------------------------------------------------------------------------
+#  Config
+# -------------------------------------------------------------------------
+# Logging config
+config.dictConfig(LOGGING_CONFIG)
+# Parser config
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--project_id', type=str,
+    help='GCP project in which the script will be executed'
+)
+parser.add_argument(
+    '--execution_date', type=str,
+    help='DAG execution date'
+)
+
+# -------------------------------------------------------------------------
+# SQL QUERIES
+# -------------------------------------------------------------------------
+
+SQL_QUERIES = {
+    'TRANSACCIONES_PA' :
+    """
+    SELECT STORE_ID AS `Store Code`,
+        Modelo,
+        Zone as Zona,
+        store_name as Store_Name,
+        region as Region,
+        transacciones_actual as Transacciones,
+        transacciones_PA as `Transacciones PA`,
+        Var_transacciones_PA as `Var % Transacciones Totales`,
+        transacciones_PG_actual as `Transacciones Pan Granel`,
+        transacciones_PG_PA as `Transacciones Pan Granel PA`,
+        Var_transacciones_PG_PA as `Var % Transacciones Pan Granel`,
+        venta_bruta_PG_actual as `Venta Bruta Pan Granel`,
+        venta_bruta_PG_PA as `Venta Bruta Pan Granel PA`,
+        Var_venta_PG_PA as `Var % Venta Brutal Pan Granel`,
+        penetracion_actual as `Penetración`,
+        penetracion_PA as `Penetración PA`,
+        Var_penetracion_PA as `Var % Penetración`,
+        peso_PG_actual as `Peso Pan Granel`,
+        peso_PG_PA as `Peso Pan Granel PA`,
+        Var_peso_PG_PA as `Var % Peso Pan Granel`,
+        peso_por_visita_actual as `Peso por Visita Pan Granel`,
+        peso_por_visita_PA as `Peso por Visita Pan Granel PA`,
+        Var_peso_por_visita_PG_PA as `Var % Peso por Visita Pan Granel`,
+        frecuencia_actual as Frecuencia,
+        frecuencia_PA as `Frecuencia PA`,
+        Var_frecuencia_PA as `Var % Frecuencia`
+    FROM
+    `cl-cda-unidata-dev.DS_UNIDATA_PROVEEDORES.PROVEEDORES_PROSEPAN_VENTAS_TRANSACCIONES_PENETRACION`
+
+    WHERE SEMANA_ACTUAL_2 = '202602'
+    """,
+    'TRANSACCIONES_AA' : """
+    SELECT STORE_ID AS `Store Code`,
+        Modelo,
+        Zone as Zona,
+        store_name as Store_Name,
+        region as Region,
+        transacciones_actual as Transacciones,
+        transacciones_AA as `Transacciones AA`,
+        Var_transacciones_AA as `Var % Transacciones Totales`,
+        transacciones_PG_actual as `Transacciones Pan Granel`,
+        transacciones_PG_AA as `Transacciones Pan Granel AA`,
+        Var_transacciones_PG_AA as `Var % Transacciones Pan Granel`,
+        venta_bruta_PG_actual as `Venta Bruta Pan Granel`,
+        venta_bruta_PG_AA as `Venta Bruta Pan Granel AA`,
+        Var_venta_PG_AA as `Var % Venta Brutal Pan Granel`,
+        penetracion_actual as `Penetración`,
+        penetracion_AA as `Penetración AA`,
+        Var_penetracion_AA as `Var % Penetración`,
+        peso_PG_actual as `Peso Pan Granel`,
+        peso_PG_AA as `Peso Pan Granel AA`,
+        Var_peso_PG_AA as `Var % Peso Pan Granel`,
+        peso_por_visita_actual as `Peso por Visita Pan Granel`,
+        peso_por_visita_AA as `Peso por Visita Pan Granel AA`,
+        Var_peso_por_visita_PG_AA as `Var % Peso por Visita Pan Granel`,
+        frecuencia_actual as Frecuencia,
+        frecuencia_AA as `Frecuencia AA`,
+        Var_frecuencia_AA as `Var % Frecuencia`
+    FROM
+    `cl-cda-unidata-dev.DS_UNIDATA_PROVEEDORES.PROVEEDORES_PROSEPAN_VENTAS_TRANSACCIONES_PENETRACION`
+
+    WHERE SEMANA_ACTUAL_2 = '202602'
+    """,
+}
+
+# -------------------------------------------------------------------------
+# Main function
+# -------------------------------------------------------------------------
+def main() -> None:  # noqa: D103
+    # Parse input variables
+    args = vars(parser.parse_args())
+    gcp_project_id: str = args['project_id']
+    execution_date: str = args['execution_date']
+    transacciones = ['TRANSACCIONES_PA','TRANSACCIONES_AA']
+    # Set all clients
+
+    excel_transacciones = f'Transacciones Venta y Penetración {execution_date}.xlsx'
+    for hoja in transacciones:
+        #Read Query
+        logging.info(f'Reading Query for {hoja}')
+        transacciones_df = readBigQuery(
+            query=SQL_QUERIES[hoja],
+            user='csotob',
+            gbq_client = Client()
+        )
+
+        #Create excel from dataframe
+        logging.info(f'Creating excel file for Transacciones Prosepan {execution_date}')
+
+        offset_buffer = io.BytesIO()
+        with pd.ExcelWriter(offset_buffer, engine='openpyxl') as writer:
+            transacciones_df.to_excel(
+            writer,
+            sheet_name=hoja,
+            index=False,
+            header=True
+            )
+        file_content = offset_buffer.getvalue()
+
+        logging.info(f'Starting upload of {excel_transacciones} into SharePoint')
+
+        file_site = '/sites/BigDatayAdvancedAnalytics/Documentos compartidos/Proveedores/Prosepan'
+        input_file =  f'{file_site}/{excel_transacciones}'
+        sp_cred = secretmanager.getSecret(
+            'bdaa_sharepoint_credentials',
+            project=gcp_project_id
+        )
+        sharepoint = sp.SharePointFile(
+            **sp_cred,
+            server_relative_path=input_file
+        )
+        sharepoint.upload(file_content)
+
+        logging.info(f'removing {excel_transacciones} from local')
+        os.remove(f'{excel_transacciones}')
+    logging.info('Process ended!')
+
+if __name__ == '__main__':
+    main()
