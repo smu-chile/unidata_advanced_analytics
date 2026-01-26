3b99f477fa445d13b40235e04ed3146de06828a0
csotob-smu <csotob@smu.cl>
2026-01-26 10:04:50 -0300

    MOD: add column selection in case of unexpected changes c:

--- src/margins/scripts/margins_misc.py
+++ src/margins/scripts/margins_misc.py
@@ -66,8 +66,10 @@ def cleaning_func(file: str,df: pd.DataFrame) -> pd.DataFrame :
     file : str
         Short name of the type of file associated to the dataframe
     """
-    logging.info('Before cleaning:', df)
+    logging.info(f'Before cleaning: {df}')
     if file == 'ppto':
+        df = df[['Formato', 'Mes', 'Categoria', 'Venta_Neta', 'Costo_Neto',
+       'Margen Comercial 1', 'Recupero', 'Sell Out']]
         float_columns = 	['Venta_Neta',
                     'Costo_Neto',
                     'Margen Comercial 1',
@@ -79,7 +81,7 @@ def cleaning_func(file: str,df: pd.DataFrame) -> pd.DataFrame :
         df['año'] = ppto_year
         df['Mes'] = df['año'] + df['Mes'].map(month_dict)
         df['año'] = df['año'].astype('Int64')
-        logging.info('After cleaning:', df)
+        logging.info(f'After cleaning: {df}')
         return df
     if file == 'ppto_mg1':
         df.columns = ['Mes', 'Formato', 'Categoria', 'ppto_venta_neta',
@@ -93,18 +95,18 @@ def cleaning_func(file: str,df: pd.DataFrame) -> pd.DataFrame :
         df['año'] = ppto_year
         df['Mes'] =  pd.to_datetime(df['Mes'], format='%Y-%m-%d').dt.strftime('%Y%m')
         df['año'] = df['año'].astype('Int64')
-        logging.info('After cleaning:', df)
+        logging.info(f'After cleaning: {df}')
         return df
     if file.startswith('admg'):
         df['MES'] = df['MES'].astype('Int64')
         df['Material'] = df['Material'].astype('Int64')
         df['SELLOUT'] = df['SELLOUT'].astype('Float64')
         df = df.dropna(axis=0,subset=['MES'])  # noqa: PD901
-        logging.info('After cleaning:', df)
+        logging.info(f'After cleaning: {df}')
         return df
     if file == 'est_com_alvi':
         df = df.replace('#N/D', '')  # noqa: PD901
-        logging.info('After cleaning:', df)
+        logging.info(f'After cleaning: {df}')
         return df
     return df
 
