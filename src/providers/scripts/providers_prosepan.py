745a885e6b0316b6a82c504da17a3ddd3f15d5b2
csotob-smu <csotob@smu.cl>
2026-01-29 11:00:58 -0300

    check empty df

--- src/providers/scripts/providers_prosepan.py
+++ src/providers/scripts/providers_prosepan.py
@@ -157,7 +157,8 @@ def main() -> None:  # noqa: D103
         **sp_cred,
         server_relative_folder=file_site
     )
-    sharepoint.upload_file(input_file,file_content)
+    if not transacciones_df.empty():
+        sharepoint.upload_file(input_file,file_content)
 
     logging.info('Process ended!')
 
