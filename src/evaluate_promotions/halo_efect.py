5e84c958937515cdcb4fa10f825df1769b7849ed
Andres Bravo Medina <abravom@unidata.cl>
2026-02-03 07:54:23 -0300

    halo efect memoria

--- src/evaluate_promotions/halo_efect.py
+++ src/evaluate_promotions/halo_efect.py
@@ -127,8 +127,8 @@ with DAG(**dag_args) as dag:
                 'common/',
                 f'{PROJECT_NAME}/gbq_objects/'
             ],
-            spark_driver_cores=16,
-            spark_driver_memory=60
+            spark_driver_cores=8,
+            spark_driver_memory=35
         )
 
         promotions_sharepoint_task >> main_promotions_task
