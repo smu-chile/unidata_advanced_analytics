40c7db417138cdaf9cfa875a8f77119988f4b386
smu-ecastrot <ecastrot@unidata.cl>
2026-02-03 10:12:13 -0300

    Unlock other formats

--- src/w2v_sku_embeddings/w2v_sku_embeddings_train.py
+++ src/w2v_sku_embeddings/w2v_sku_embeddings_train.py
@@ -64,9 +64,9 @@ dag_args = {
 with DAG(**dag_args) as dag:
     EXECUTION_DATE = "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_end).strftime('%Y-%m-%d')) }}"  # noqa: E501
     STORE_BANNERS = [
-        #'Unimarc',
-        #'Mayorista',
-        #'Alvi',
+        'Unimarc',
+        'Mayorista',
+        'Alvi',
         'Super 10'
     ]
 
