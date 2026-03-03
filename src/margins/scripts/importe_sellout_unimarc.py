14df664aefb39ceb3711b254a2ab41ce102641d6
csotob-smu <csotob@smu.cl>
2026-03-03 08:25:00 -0300

    itest

--- src/margins/scripts/importe_sellout_unimarc.py
+++ src/margins/scripts/importe_sellout_unimarc.py
@@ -79,7 +79,7 @@ def cleaning_func(df_file:pd.DataFrame,mes:str,semana_carga:str)->pd.DataFrame:
     df_file = df_file.replace('nan', '')
     df_file['importe_sell_out'] = df_file['importe_sell_out'].astype('Float64').astype('Int64')
     df_file['factor'] = df_file['factor'].astype('Float64').astype('Int64')
-    df_file['pvp_normal'] = df_file['pvp_normal'].astype('Int64')
+    df_file['pvp_normal'] = pd.to_numeric(df_file['pvp_normal'])
     #Agregar semana carga y mes datos
     df_file['mes'] = pd.to_datetime(mes,format='%Y%m')
     df_file['semana_carga'] = semana_carga
