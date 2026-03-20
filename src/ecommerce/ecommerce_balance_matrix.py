046aa5e87458c1c7c87d7fe46796ce336c5a736e
Andres Bravo Medina <abravom@unidata.cl>
2026-03-20 09:37:43 -0300

    ecommerce balance matrix ajuste dag parametros

--- src/ecommerce/ecommerce_balance_matrix.py
+++ src/ecommerce/ecommerce_balance_matrix.py
@@ -115,6 +115,7 @@ with DAG(**dag_args) as dag:
                 '--project_id', dag_env_config['project_id'],
                 '--execution_date', EXECUTION_DATE,
                 '--store_banner',store_banner,
+                '--use','ELASTICITY'
             ],
             include_paths=[
                 'common/',
