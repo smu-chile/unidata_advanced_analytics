b8a7364e6866021833db5c8ea5334ef37ecdf473
Andres Bravo Medina <abravom@unidata.cl>
2026-05-18 15:23:54 -0400

    ecommerce my usuals

---
+++ src/ecommerce_my_usuals/my_usuals.py
@@ -0,0 +1,99 @@
+"""Defines the DAG that allocates my usuals products."""
+# Default
+import json
+import platform
+import importlib
+from datetime import timedelta
+
+# pip
+import pendulum
+from airflow.models import DAG
+from airflow.configuration import conf
+from airflow.models.baseoperator import chain
+
+
+if platform.system() == 'Windows':
+    from common.operators.dataproc_create_batch import (
+        ExtendedDataprocCreateBatchOperator,
+    )
+elif platform.system() == 'Linux':
+    ExtendedDataprocCreateBatchOperator = (importlib.import_module(
+        'BRANCH_PLACEHOLDER.'
+        'smu-chile.unidata_advanced_analytics.'
+        'src.common.operators.dataproc_create_batch'
+    )).ExtendedDataprocCreateBatchOperator
+else:
+    err_msg = 'Only Linux and Windows are supported.'
+    raise NotImplementedError(err_msg)
+
+# Globals
+with open(
+    f'{conf.get("core", "dags_folder")}/'
+    'BRANCH_PLACEHOLDER/'
+    'smu-chile/unidata_advanced_analytics/src/common/constants/dag_env_config.json'
+) as f:
+    dag_env_config = json.load(f)['BRANCH_PLACEHOLDER']
+
+PROJECT_NAME = 'ecommerce_my_usuals'
+
+dag_args = {
+    'dag_id': 'my_usuals_allocation',
+    'schedule_interval': '0 9 * * 3',
+    'dagrun_timeout': None,
+    'catchup': False,
+    'max_active_runs': 1,
+    'concurrency': 2,
+    'tags': [PROJECT_NAME,'abravom'],
+    'default_args': {
+        'project_id': dag_env_config['project_id'],
+        'region': dag_env_config['region'],
+        'owner': 'BIGDATA_ANALYTICS',
+        'email': ['abravom@unidata.cl'],
+        'start_date': pendulum.datetime(
+            2023, 12, 5,
+            tz=pendulum.timezone('America/Santiago')
+        ),
+        'depends_on_past': False,
+        'catchup': False,
+        'email_on_failure': True,
+        'email_on_retry': False,
+        'retries': 0,
+        'retry_delay': timedelta(minutes=5)
+    }
+}
+
+with DAG(**dag_args) as dag:
+    train_tasks = [
+        ExtendedDataprocCreateBatchOperator(
+            task_id=f"compute_score_{store_banner.replace(' ', '_').lower()}",
+            python_script_path=(
+                f'{PROJECT_NAME}/'
+                'scripts/'
+                'my_usuals.py'
+            ),
+            dag_env_config=dag_env_config,
+            docker_image_name=f'{PROJECT_NAME}',
+            pyspark_batch_args=[
+                '--project_name', PROJECT_NAME,
+                '--gcp_project', dag_env_config['project_id'],
+                '--execution_date', "{{ dag_run.conf.get('execution_date', dag.timezone.convert(data_interval_end).next(3).strftime('%Y-%m-%d')) }}",  # noqa: E501
+                '--store_banner', store_banner,
+                '--rollback_months', "{{ dag_run.conf.get('rollback_months', 6) }}",
+                '--rollback_months_filter', "{{ dag_run.conf.get('rollback_months_filter', 12) }}",
+                '--min_transacted_months', "{{ dag_run.conf.get('min_transacted_months', 2) }}",
+                '--top_n', "{{ dag_run.conf.get('top_n', 100) }}",
+            ],
+            include_paths=[
+                'common/',
+                f'{PROJECT_NAME}/gbq_objects/'
+            ],
+        )
+
+        for store_banner in [
+            'Unimarc',
+            'Alvi',
+        ]
+    ]
+
+
+chain(train_tasks)
