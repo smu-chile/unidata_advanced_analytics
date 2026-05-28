0f5f0ec8fd88ccce78ef84973f5eb816a79655b2
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-05-28 08:28:27 -0400

    forecast 41 meses

--- src/pricing/scripts/processed_regression_data.py
+++ src/pricing/scripts/processed_regression_data.py
@@ -417,12 +417,14 @@ def main() -> None:  # noqa: D103
     store_banner_tabla = 'Super_10' if store_banner == 'Super 10' else store_banner
 
     esquema = 'TMP'
-    tabla = f'TMP_REGRESSION_DATA_{use}_aux_{store_banner_tabla}'
+    #PARCHE TEMPORAL
+    tabla = f'TMP_REGRESSION_DATA_{use}_aux_{store_banner_tabla}_EXPANDED'
     tmp_path_table_aux = f'{proyecto}.{esquema}.{tabla}'
 
     # Tabla temporal final
     esquema = 'TMP'
-    tabla = f'TMP_REGRESSION_DATA_{use}'
+    # PARCHE TEMPORAL
+    tabla = f'TMP_REGRESSION_DATA_{use}_EXPANDED'
     tmp_path_table = f'{proyecto}.{esquema}.{tabla}'
 
 
@@ -451,7 +453,8 @@ def main() -> None:  # noqa: D103
     #----------------------------------------------------------------------
 
     # Cantidad de meses
-    cant_meses = 29
+    # Parche temporal
+    cant_meses = 29 + 12
 
     if use == 'FORECAST':
         # Convertir fecha de ejecución
