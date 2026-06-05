9159c8389e21e7a923dd68ae9798321c7129bffa
Andres Bravo Medina <abravom@unidata.cl>
2026-06-05 08:42:19 -0400

    my usuals ajuste query

--- src/my_usuals/scripts/my_usuals.py
+++ src/my_usuals/scripts/my_usuals.py
@@ -124,7 +124,7 @@ SQL_QUERIES = QueryDict({
         )
         USING (customer_key)
         WHERE in_filter IS NULL
-            AND unimarc_logged = 1
+            AND ${store_banner}_logged = 1
         GROUP BY 1
 
         UNION ALL
@@ -945,7 +945,7 @@ SQL_QUERIES = QueryDict({
         )
         USING (customer_key)
         WHERE in_filter IS NULL
-            AND unimarc_logged = 1
+            AND ${store_banner}_logged = 1
         GROUP BY 1
 
         UNION ALL
