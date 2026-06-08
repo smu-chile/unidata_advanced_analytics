7771dc6d3bdf1bf4ab55abc8ca3dab3207f914f8
Ivan Lopez Espinoza <ilopeze@unidata.cl>
2026-06-08 16:43:41 -0400

    Cambios finales smfc publist sftp to bq

---
+++ src/ingest/sftp/scripts/salesforce_sfmc_publist_stg_to_bq.py
@@ -0,0 +1,132 @@
+import json
+import logging
+from pathlib import Path
+
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+PROJECT_ID = 'cl-bigdata-analytics-preprod'
+DATASET_ID = 'CRM'
+STG_TABLE = (f'{PROJECT_ID}.{DATASET_ID}.CRM_DATA_SFMC_PUBLIST_STG')
+FINAL_TABLE = (f'{PROJECT_ID}.{DATASET_ID}.CRM_DATA_SFMC_PUBLIST')
+
+JSON_FILE = (
+    Path(__file__).parents[1]
+    / 'gbq_objects'
+    / 'CRM_DATA_SFMC_PUBLIST.json')
+
+def build_select(columns):
+
+    select_fields = []
+
+    for column in columns:
+        field_name = column['name']
+
+        field_type = (
+            column['field_type']
+            .upper()
+            .strip()
+        )
+        if field_name == 'FECHA_CARGA':
+            expression = 'CURRENT_DATE() AS FECHA_CARGA'
+        elif field_type == 'STRING':
+            expression = (
+                f'TRIM({field_name}) '
+                f'AS {field_name}'
+            )
+        elif field_type in (
+            'INT64',
+            'INTEGER'
+        ):
+            expression = (
+                f'SAFE_CAST('
+                f'NULLIF(TRIM({field_name}), "") '
+                f'AS INT64'
+                f') AS {field_name}'
+            )
+        elif field_type == 'FLOAT64':
+            expression = (
+                f'SAFE_CAST('
+                f'NULLIF(TRIM({field_name}), "") '
+                f'AS FLOAT64'
+                f') AS {field_name}'
+            )
+        elif field_type == 'NUMERIC':
+            expression = (
+                f'SAFE_CAST('
+                f'NULLIF(TRIM({field_name}), "") '
+                f'AS NUMERIC'
+                f') AS {field_name}'
+            )
+        elif field_type == 'DATE':
+            expression = (
+                f'SAFE_CAST('
+                f'NULLIF(TRIM({field_name}), "") '
+                f'AS DATE'
+                f') AS {field_name}'
+            )
+        elif field_type == 'DATETIME':
+            expression = (
+                f'CASE '
+                f'WHEN NULLIF(TRIM({field_name}), "") IS NULL '
+                f'THEN NULL '
+                f'ELSE PARSE_DATETIME('
+                f'"%b %e %Y %l:%M%p",'
+                f'TRIM({field_name})'
+                f') '
+                f'END AS {field_name}'
+            )
+        elif field_type == 'TIMESTAMP':
+            expression = (
+                f'SAFE_CAST('
+                f'NULLIF(TRIM({field_name}), "") '
+                f'AS TIMESTAMP'
+                f') AS {field_name}'
+            )
+        else:
+            expression = (
+                f'{field_name}'
+            )
+        select_fields.append(expression)
+    return ',\n'.join(select_fields)
+def main():
+    logging.info(f'Loading schema: {JSON_FILE}')
+
+    with open(JSON_FILE, encoding='utf-8') as f:
+        metadata = json.load(f)
+
+    columns = metadata['columns']
+
+    client = bigquery.Client(project=PROJECT_ID)
+
+    try:
+        client.get_table(FINAL_TABLE)
+        logging.info('Target table exists')
+
+    except NotFound:
+        raise Exception(f'Table not found: {FINAL_TABLE}')  # noqa: B904, EM102
+
+    select_sql = (build_select(columns))
+
+    query = f"""
+    TRUNCATE TABLE `{FINAL_TABLE}`;
+    INSERT INTO `{FINAL_TABLE}`
+    SELECT
+    {select_sql}
+    FROM `{STG_TABLE}`
+    """  # noqa: S608
+
+    logging.info('Executing transformation')
+    job = client.query(query)
+    job.result()
+    logging.info('Process completed successfully')
+
+
+if __name__ == '__main__':
+    main()
