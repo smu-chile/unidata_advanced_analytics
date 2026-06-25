842da43cbebeed9cbfa625c822d0c1389e293485
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-06-25 14:40:02 -0400

    double quotes issues

--- src/balance_matrix_stores_id/scripts/balance_matrix_stores_id.py
+++ src/balance_matrix_stores_id/scripts/balance_matrix_stores_id.py
@@ -149,7 +149,7 @@ def main() -> None:  # noqa: D103
     else:
         suffix = f"{len(store_id_list)}_stores"
 
-    suffix = suffix.replace(" ", "_").lower()
+    suffix = suffix.replace(' ', '_').lower()
 
     store_id_sql = ','.join(f"'{s}'" for s in store_id_list)
     store_id_str = ','.join(store_id_list)
