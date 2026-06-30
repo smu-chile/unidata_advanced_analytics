9e7b9ede330ab69e2b11c1265167ea31854a9fa5
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-06-30 10:13:25 -0400

    ruff issues

--- src/balance_matrix_stores_id/scripts/balance_matrix_stores_id.py
+++ src/balance_matrix_stores_id/scripts/balance_matrix_stores_id.py
@@ -151,7 +151,6 @@ def main() -> None:  # noqa: D103
 
     suffix = suffix.replace(' ', '_').lower()
 
-    store_id_sql = ','.join(f"'{s}'" for s in store_id_list)
     store_id_str = ','.join(store_id_list)
 
     logging.info(f'execution_date: {execution_date}')
