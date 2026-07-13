9c0db5be603ac2111d3fa32380f44791845d8566
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-07-13 13:53:32 -0400

    vis col error

--- src/ecommerce_balance_matrix/scripts/balance_matrix_ecommerce.py
+++ src/ecommerce_balance_matrix/scripts/balance_matrix_ecommerce.py
@@ -300,6 +300,7 @@ def main() -> None:  # noqa: D103
 
 
         for i, col in enumerate(columnas):
+            print('col: ',col, 'type: ', type(col))
             serie = df_balance_matrix_sp[col].astype(str)
             max_len = max(serie.map(len).max(), len(col))
             width = max_len + 2
