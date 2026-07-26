f9f825da4bad6a4c03acadd01db7ace266787869
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-07-26 02:15:07 -0400

    replace to append

--- src/balance_matrix_stores_id/scripts/processed_regression_data_stores_id.py
+++ src/balance_matrix_stores_id/scripts/processed_regression_data_stores_id.py
@@ -1075,7 +1075,7 @@ def main() -> None:  # noqa: D103
                                             nombre_json),
             project=proyecto,
             gbq_client=gbq_client,
-            if_exists='replace' #elimina-inserta. 'append' sobrescribe, si no existe crea la tabla.
+            if_exists='append' #elimina-inserta. 'append' sobrescribe, si no existe crea la tabla.
         )
 
     logging.info(f'Se sube info actualizada del formato {store_banner}...')
