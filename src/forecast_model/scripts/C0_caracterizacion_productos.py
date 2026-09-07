a8f317ffff99a6274b5356705ae9c3971c21aa75
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-09-07 11:39:21 -0300

    batallando contra ruff

--- src/forecast_model/scripts/C0_caracterizacion_productos.py
+++ src/forecast_model/scripts/C0_caracterizacion_productos.py
@@ -3,22 +3,21 @@ from __future__ import annotations
 
 import os
 import sys
+import logging
 from string import Template
+from logging import config
 from pathlib import Path
 from datetime import datetime
 from textwrap import dedent
-
-import logging
-from logging import config
 
 # Pip
 import numpy as np
 import pandas as pd
 import pendulum
-import matplotlib.pyplot as plt
 from boto3 import Session
 from google.cloud.bigquery import Client
 from dateutil.relativedelta import relativedelta
+
 
 directorio_actual = os.path.abspath(os.curdir)
 
@@ -31,6 +30,12 @@ while directorio_actual != os.path.sep:
         sys.path.pop()  # Remueve el directorio que no contenía el módulo
         directorio_actual = os.path.dirname(directorio_actual)  # Retrocede
 
+import posixpath
+
+import statsmodels.api as sm
+
+import common.gcp_extended.secretsmanager as secretmanager
+import common.office365_extended.sharepoint as sp
 from common.constants import LOGGING_CONFIG
 from common.databases.queries import QueryDict  # noqa: E402
 from common.gcp_extended.bigquery import (  # noqa: E402
@@ -40,18 +45,36 @@ from common.gcp_extended.bigquery import (  # noqa: E402
     createTableAsSelect,  # noqa: F401
 )
 
-import common.gcp_extended.secretsmanager as secretmanager
-import common.office365_extended.sharepoint as sp
-import posixpath
-import re
-import statsmodels.api as sm
 
 sys.path.append(os.path.abspath('..'))
-from scripts.forecast_promotion_auditoria import generarDataFrame
+from config_forecast import tabla, gbq_client, path_table, store_banner  # noqa: E402
+from scripts.forecast_promotion_auditoria import generarDataFrame  # noqa: E402
 
-from config_forecast import (
-        tabla, 
-        path_table, 
-        store_banner, 
-        gbq_client
+
+#######################################################
+##########---------- 0. Auxiliares ----------##########
+#######################################################
+
+
+#########           0.1 Queries
+
+# Query para obtener la data procesada de forecast para todos los
+# productos disponibles.
+
+QUERY_FORECAST= QueryDict({
+    'query_data_procesada':
+    """
+
+    SELECT * EXCEPT(
+        PRIMER_DIA_MES,
+        ULTIMO_DIA_MES,
+        P_WEEK,
+        P_MONTH,
+        VARIACION_TOP1_SUSTITUTO,
+        VARIACION_TOP3_SUSTITUTOS
         )
+
+    FROM `${path_table}`
+    Where store_banner = 'Unimarc'
+    """})
+
