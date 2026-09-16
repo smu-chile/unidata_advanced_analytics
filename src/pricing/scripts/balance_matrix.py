5ea543119e9bac63ffa3e7ed0d55ca6982abff51
Ruben Lagos Godoy <rlagosg@unidata.cl>
2026-09-16 10:01:09 -0300

    incertidumbre

--- src/pricing/scripts/balance_matrix.py
+++ src/pricing/scripts/balance_matrix.py
@@ -453,7 +453,7 @@ def main() -> None:  # noqa: D103
     print('#(IS nans) agregados: ', mask_fillna_is.sum())
     print('#(ISF nans) agregados: ', mask_fillna_isf.sum())
 
-    df_balance_matrix['kvi'] = df_balance_matrix['kvi'].fillna('BKG')
+    df_balance_matrix['kvi'] = df_balance_matrix['KVI'].fillna('BKG')
 
     #Parche: agregamos columna subcat description
     df_balance_matrix = df_balance_matrix.merge(
